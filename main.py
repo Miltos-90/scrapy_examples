cb5a2169080e645c88883faf89c3f0121e87c57a
Miltos-90 <35740991+Miltos-90@users.noreply.github.com>
2023-02-12 10:52:47 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -14,6 +14,29 @@ clean up / comment extension
 check how not to be banned
 """
 
+# ===============================================================
+def clean():
+    import os
+    paths = ['./logger.log', './scrapy_quotes.db', './visited_urls.db']
+    for filePath in paths:
+        if os.path.isfile(filePath): 
+            os.remove(filePath)
+
+#clean()
+# ===============================================================
+
+"""
+Downloadermiddleware:
+record request from spider
+record response
+if response is an error (see here: https://developer.mozilla.org/en-US/docs/Web/HTTP/Status#client_error_responses)
+retry based on error (in spider)
+else get next link directly (in spider)
+
+"""
+
+        
+
 from scrapy.crawler import CrawlerProcess
 from quotes.spiders.quotes_spider import QuotesSpider
 from scrapy.utils.project import get_project_settings
