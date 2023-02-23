2ef5383a3ddbd24d9b237f6e31e43ed288b8fa95
Miltos-90 <35740991+Miltos-90@users.noreply.github.com>
2023-02-23 20:51:26 +0100

    Add files via upload

--- resources/ua_generator/software.py
+++ resources/ua_generator/software.py
@@ -1,12 +1,18 @@
 from random import randint, choice
 import software_data as data
 import string
+from dataclasses import dataclass
 
-""" TODO
-Make software object which contains name/version/details fields.
-Have the software generators return a software
+@dataclass
+class Software():
+    name    : str  = None
+    version : str  = None
+    details : dict = None
 
-"""
+@dataclass
+class User():
+    browser : Software = None
+    os      : Software = None
 
 
 class SoftwareGenerator():
@@ -21,7 +27,7 @@ class SoftwareGenerator():
         return
 
 
-    def __call__(self, name: str) -> dict:
+    def __call__(self, name: str) -> Software:
         """ Randomly choose a software version and its associated details"""
         
         versions = self.versions[name] # Get versions relevant to software <name>
@@ -32,22 +38,24 @@ class SoftwareGenerator():
 
         # Select a version from those
         majorVer = choice(list(versions.keys()))  
-        details  = versions[majorVer]
 
-        return self.makeDetails(majorVer, details)
+        return Software(
+            name    = name,
+            version = majorVer,
+            details = self.makeDetails(versions[majorVer])
+        )
 
 
     @staticmethod
-    def makeDetails(key, props):
+    def makeDetails(props):
         """ Generates all addditional details of a major version of a software """
 
-        d = {'major': key} # Add major version to the dict
-
         # Loop over all properties and populate dictionary
+        d = {}
         for propKey, propValue in props.items():
                     
             if propKey == 'minor_range': 
-                d['minor'] = randint(propValue[0], propValue[1])
+                d['minor_version'] = randint(propValue[0], propValue[1])
             else:
                 d[propKey] = propValue
 
@@ -69,11 +77,15 @@ class AndroidOSGenerator(SoftwareGenerator):
 
         # Randomly select a brand of mobile phones operating on Android and make OS
         if brand is None: brand = choice(self.names)
-        os = super(AndroidOSGenerator, self).__call__(brand)
+        
+        os      = super(AndroidOSGenerator, self).__call__(brand)
+        os.name = 'android'
 
         # Make Android-specific details
-        os['build_number'] = self._buildNumber(os['build_number'])
-        os['device_name']  = choice(self.devices[brand])
+        
+        os.details['brand']        = brand
+        os.details['build_number'] = self._buildNumber(os.details['build_number'])
+        os.details['device_name']  = choice(self.devices[brand])
 
         return os
     
@@ -104,58 +116,123 @@ class AndroidOSGenerator(SoftwareGenerator):
         )
 
 
-class PlatformGenerator():
+class UserGenerator():
     """ Generates a random OS and a browser. """
 
     def __init__(self,
-        osVersions : dict = data.operating_systems,
-        brVersions : dict = data.browsers,
-        androidSys : dict = {
+        osVersions  : dict = data.operating_systems,
+        brVersions  : dict = data.browsers,
+        uaTemplates : dict = data.templates,
+        androidSys  : dict = {
             'versions' : data.android.versions,
             'devices'  : data.android.devices
         }
     ):
 
         self.os        = SoftwareGenerator(osVersions)
-        self.browser   = SoftwareGenerator(brVersions)
         self.androidOs = AndroidOSGenerator(androidSys)
+        self.browser   = SoftwareGenerator(brVersions)
         
         # List of OS and browsers available
-        self.osNames = self.os.names + ['android']
-        self.brNames = self.browser.names
+        self.combinations = self._validCombinations(uaTemplates)
+        self.osNames      = list(self.combinations.keys())
         
+        # User agent templates
+        self.templates = uaTemplates
 
-    def __call__(self, osName:str = None, brName: str = None):
+        return
+
+    def __call__(self, osName:str = None, brName: str = None) -> User:
         """ Either returns an an OS and a browser  with the given 
             names or chooses both at random
         """
 
         if osName is None: osName = choice(self.osNames)
+        if brName is None: brName = choice(self.combinations[osName])
 
-        if osName != 'android' : os = {'name': osName, 'version': self.os(osName)}
-        else                   : os = {'name': osName, 'version': self.androidOs()}
-
-        if brName is None: brName = choice(self.brNames)
-        browser = {'name': brName, 'version': self.browser(brName)}
-
-        return Platform(os, browser)
-
-
-class Platform():
-
-    def __init__(self, os, browser):
-        self.os        = os
-        self.browser   = browser
+        user = User(os = self._makeOS(osName), browser = self.browser(brName))
         
+        self._makeAgent(user)
+
+        return user
+
+    def _makeOS(self, name) -> Software:
+        """ Generates an operating system given its name. """
+
+        return self.androidOs() if name == 'android' else self.os(name)
+
+    def _makeAgent(self, user: User):
+
+        # Select a template (if multiples exist)
+        template = choice(self.templates[(user.os.name, user.browser.name)])
+
+        # Replace os version
+        template = template.replace('{windows}', user.os.version)
+        template = template.replace('{linux}', user.os.version)
+        template = template.replace('{android}', user.os.version.replace('.0', ''))
+        template = template.replace('{macos}', version(user.os, strip_zero = True).replace('.', '_'))
+        template = template.replace('{ios}', version(user.os, strip_zero = True).replace('.', '_'))
+                
+        # Replace browser version
+        template = template.replace('{chrome}',  version(user.browser))
+        template = template.replace('{firefox}', version(user.browser, strip_zero = True))
+        template = template.replace('{safari}', version(user.browser))
+
+        # Replace version details
+        template = template.replace('{webkit}', user.browser.details.get('webkit', ''))
+        template = template.replace('{device}', '; ' + user.os.details.get('device_name', ''))
+        template = template.replace('{build}', '; Build/' + user.os.details.get('build_number', ''))
+        
+        
+        print(template)
+
+        return
+        
+
+
+    @staticmethod
+    def _validCombinations(templates):
+        """ Generates a dictionary with valid os and browser combinations.
+            key: operating system, value: list with valid browsers
+        """
+
+        d = {}
+        for os, browser in templates.keys():
+
+            if os not in d: 
+                d[os] = [] # Make an empty list
+
+            else:           
+                d[os].append(browser) # Append
+
+        return d
+
+
+def version(os, strip_zero=False):
+
+    minorVersion = os.details.get('minor_version')
+    
+    c = minorVersion and (not strip_zero or (strip_zero and minorVersion > 0) )
+
+    v_str = str(os.version)
+    if c:
+        v_str += '.' + str(minorVersion)
+
+    return v_str
+
+
+def major_version(version_dict):
+    return str(version_dict['major']).split('.', 1)[0]
+
 
 if __name__ == '__main__':
 
-    pg = PlatformGenerator()
+    pg = UserGenerator()
 
     for _ in range(50):
         
         p = pg()
-        print(p.os['name'], p.os['version'])
-        print(p.browser['name'], p.browser['version'])
+        print(p.os)
+        print(p.browser)
         print()
 
