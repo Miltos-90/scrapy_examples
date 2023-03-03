597f708aeadbeceba450c575cc064296f334f3f6
Miltos-90 <35740991+Miltos-90@users.noreply.github.com>
2023-03-03 17:03:42 +0100

    Add files via upload

---
+++ resources/user_agent/ua_reader/reader.py
@@ -0,0 +1,21 @@
+import os
+from random import choice
+
+class Reader():
+    """ Simpe class to read user agents from a file. It is assumed that each 
+        line of the filename corresponds to a single user agent string.
+    """
+
+    def __init__(self, filename: str):
+
+        if not os.path.isfile(filename):
+            raise FileNotFoundError(f'File {filename} does not exist.')
+        
+        with open(filename, 'r') as f: x = f.readlines()
+        
+        self.agents = [e.strip()for e in x] # Remove leading/trailing spaces
+
+        return
+
+    # Return a randomly selected user agent
+    def __call__(self): return choice(self.agents)
