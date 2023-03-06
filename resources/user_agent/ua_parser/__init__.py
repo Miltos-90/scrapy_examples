866bfbeaed9131f4987a3e9850ebd2efbdc7943f
Miltos-90 <35740991+Miltos-90@users.noreply.github.com>
2023-03-06 20:38:10 +0100

    Delete resources directory

--- resources/user_agent/ua_parser/__init__.py
+++
@@ -1,9 +0,0 @@
-from .parser import Parser
-
-""" Compile all regexes when imported """
-from .constants import BROWSER, CPU, DEVICE, ENGINE, OS
-import re
-
-for list_ in [BROWSER, CPU, DEVICE, ENGINE, OS]:
-    for dict_ in list_: 
-        dict_['regex'] = re.compile(dict_['regex'], re.IGNORECASE)
