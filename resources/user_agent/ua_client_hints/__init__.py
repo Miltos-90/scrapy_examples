f0f9fa7e51a0968ba560580c7be3361ca03ff607
Miltos-90 <35740991+Miltos-90@users.noreply.github.com>
2023-03-06 20:39:11 +0100

    Add files via upload

---
+++ resources/user_agent/ua_client_hints/__init__.py
@@ -0,0 +1,1 @@
+from .client_hints import ClientHintGenerator, ClientHints
---
+++ resources/user_agent/ua_parser/__init__.py
@@ -0,0 +1,9 @@
+from .parser import Parser
+
+""" Compile all regexes when imported """
+from .regexes import BROWSER, CPU, DEVICE, ENGINE, OS
+import re
+
+for list_ in [BROWSER, CPU, DEVICE, ENGINE, OS]:
+    for dict_ in list_: 
+        dict_['regex'] = re.compile(dict_['regex'], re.IGNORECASE)
