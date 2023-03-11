1b323b982a4a23eb65c85cc92beacd0b5768d24e
Miltos-90 <35740991+Miltos-90@users.noreply.github.com>
2023-03-11 20:43:39 +0100

    Delete resources directory

--- resources/definitions.py
+++
@@ -1,7 +0,0 @@
-""" Declaration of classes / datatypes used jointly by all modules """
-
-from typing import Literal
-
-BROWSER_TYPE = Literal['chrome', 'edge', 'firefox', 'safari', 'unknown'] # Available browsers
-DEVICE_TYPE  = Literal['desktop', 'mobile', 'unknown']                   # Available device types
-
